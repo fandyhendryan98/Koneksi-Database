7c028e740892e5ac27e278df623a6b2da0461632
fandyhendryan98 <fandyhendryan98@gmail.com>
2019-12-06 18:17:58 +0700

    first commit

---
+++ app/src/main/java/com/example/koneksidatabase/RVAdapterFakultas.kt
@@ -0,0 +1,22 @@
+package com.example.koneksidatabase
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RVAAdapterFakultas(private val context: Context, private val arrayList: ArrayList<Fakultas>):
+    RecyclerView.Adapter<RVAAdapterFakultas.Holder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return Holder(LayoutInflater.from(parent.context).inflate(R.layout.fakultas_list,parent,false))
+    }
+
+    override fun getItemCount(): Int = arrayList!!.size
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.view.label_kodefakultas.text = arrayList?.get(position)?.kode_fakultas
+        holder.view.label_namafakultas.text = "Nama Fakultas:"+arrayList?.get(position)?.nama_fakultas
+    }
+
+    class Holder(val view: View : RecyclerView.ViewHolder(view))
+}
